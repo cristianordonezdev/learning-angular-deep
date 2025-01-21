8328cc6e216e1da3d5ea7c218e68d7b9c4f9a014
Hernandez Ordonez <2383131@cognizant.com>
2025-01-21 13:46:01 -0600

    feature(implementing ng-content for slots)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren, AfterViewInit } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
     selector: 'app-root',
@@ -8,7 +9,21 @@ import { Course } from './model/course';
     styleUrls: ['./app.component.css'],
     standalone: false
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+    // To get the relation of any components in refs 
+    @ViewChild('card1', { static: false }) card: CourseCardComponent;
+    @ViewChild('card1', { read: ElementRef }) cardByElement: CourseCardComponent;
+    @ViewChildren(CourseCardComponent) cardsElements: QueryList<CourseCardComponent>
+    
+    ngAfterViewInit(): void {
+        console.log("Hello the cards");
+        console.log(this.cardsElements)
+        this.cardsElements.changes.subscribe((observer) => {
+            console.log("some change to the list of courses", observer)
+        })
+    }
+
     public courses: Course[] = COURSES;
 
     public date: Date = new Date();
@@ -17,7 +32,24 @@ export class AppComponent {
 
     public price: number = 83.382324
 
+
     handleClick(course: Course) {
         console.log("Hello clicked here", course)
+    }
+
+    handleClickReference(course: Course) {
+        console.log("Hello", this.card, this.cardByElement)
+
+    }
+    addListItem() {
+        this.courses.push({
+            id: 2,
+            description: "RxJs In Practice Course",
+            iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
+            longDescription: "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
+            category: 'BEGINNER',
+            lessonsCount: 10
+        })
+        console.log(this.courses)
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { ImageCourseComponent } from './image-course/image-course.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CourseCardComponent } from './course-card/course-card.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CourseCardComponent
+    CourseCardComponent,
+    ImageCourseComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
